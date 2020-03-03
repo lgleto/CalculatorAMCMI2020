d98bbc60c5e4ad2d2c494469c28e3369c69a0f2d
lgleto <leto.lg@gmail.com>
2020-03-03 12:47:17 +0000

    first commit

---
+++ app/src/main/java/ipca/example/calculator/MainActivity.kt
@@ -0,0 +1,102 @@
+package ipca.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var operand = 0.0
+    var operator = ""
+
+    var userIsInTheMiddleOfOperation = false
+
+    var hasDot = false
+
+    var displayFun : ((view:View)->Unit) = { view : View ->
+        var button = (view as Button)
+        if (userIsInTheMiddleOfOperation){
+            if(textViewDisplay.text.equals("0")&&!button.text.equals(".")){
+                textViewDisplay.text=""
+            }
+            if(button.text.equals(".")){
+                if(!hasDot) {
+                    textViewDisplay.text = "${textViewDisplay.text}."
+                    hasDot = true
+                }
+            }else {
+                textViewDisplay.text = "${textViewDisplay.text}${button.text}"
+            }
+        }else {
+            textViewDisplay.text = "${button.text}"
+            userIsInTheMiddleOfOperation = true
+        }
+
+    }
+
+    var operationFun : ((view:View)->Unit) = { view : View ->
+
+        if (operator.equals("")) {
+            operand = textViewDisplay.text.toString().toDouble()
+            operator = (view as Button).text.toString()
+            userIsInTheMiddleOfOperation = false
+        }else {
+            var secondOperand = textViewDisplay.text.toString().toDouble()
+            var result = doOperation(operand,secondOperand, operator)
+            textViewDisplay.text = "${result}"
+            userIsInTheMiddleOfOperation = false
+        }
+
+    }
+
+    fun doOperation(op1:Double,op2:Double,operator:String) : Double {
+        var result = 0.0
+        when(operator){
+            "+" -> {
+                result = op1 + op2
+            }
+            "-" -> {
+                result = op1 - op2
+            }
+            "*" -> {
+                result = op1 * op2
+            }
+            "/" -> {
+                result = op1 / op2
+            }
+        }
+        return result
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonN9.setOnClickListener(displayFun)
+        buttonN8.setOnClickListener(displayFun)
+        buttonN7.setOnClickListener(displayFun)
+        buttonN6.setOnClickListener(displayFun)
+        buttonN5.setOnClickListener(displayFun)
+        buttonN4.setOnClickListener(displayFun)
+        buttonN3.setOnClickListener(displayFun)
+        buttonN2.setOnClickListener(displayFun)
+        buttonN1.setOnClickListener(displayFun)
+        buttonN0.setOnClickListener(displayFun)
+        buttonDot.setOnClickListener(displayFun)
+
+        buttonOpPlus.setOnClickListener(operationFun)
+        buttonOpMinus.setOnClickListener(operationFun)
+        buttonOpTimes.setOnClickListener(operationFun)
+        buttonOpDivision.setOnClickListener(operationFun)
+
+        buttonOpEqual.setOnClickListener{
+            var secondOperand = textViewDisplay.text.toString().toDouble()
+            var result = doOperation(operand,secondOperand, operator)
+            textViewDisplay.text = "${result}"
+            userIsInTheMiddleOfOperation = false
+        }
+
+    }
+}
